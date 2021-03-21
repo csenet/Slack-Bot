90fb18682bbb6c4c8c2e1415802d71c2dc74da76
Koichi Hirachi <kouichi.hirachi@gmail.com>
2021-03-21 11:43:00 +0900

    バグ修正

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ controller.on("app_mention", async (bot, message) => {
     const msg = message.incoming_message.channelData.text;
     let data = msg.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
     let output: any;
-    if (data.match(/maketeam [0-9]*/)) {
+    let contri: any;
+    if (data.match(/maketeam [1-9][0-9]*/)) {
         data = data.replace(/maketeam /g, '');
         let num = Number(data);
         output = "*チーム一覧*\n";
@@ -39,6 +40,39 @@ controller.on("app_mention", async (bot, message) => {
             .catch(err => {
                 output = "Error";
             })
+    } else if (data.match(/account /)) {
+        // data1にaccount名を格納
+        data = data.replace(/account /g, '').split(' ');
+        let data1 = data[0];
+
+        // data2にroleを格納
+        let data2 = data[1];
+
+        await axios.get(`http://160.251.78.132/users/${data1}/contributions`)
+            .then(res => {
+                const data = res.data["contributions"];
+                contri = Number(data)
+            })
+            .catch(err => {
+                output = "Error";
+            })
+        await fetch("http://160.251.78.132/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify({
+                account_name: data1,
+                role: data2,
+                contribution: contri
+            })
+        }).then(function (response) {
+            // レスポンス結果
+            console.log(response)
+        }, function (error) {
+            // エラー内容
+            console.error(error.message)
+        });
     } else {
         output = "*コマンド一覧*\nmaketeam {1チームの人数)：チームリストを表示\n"
     }
